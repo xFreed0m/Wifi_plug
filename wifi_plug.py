2de5da9483ba2a3d7c88cba83ebcd19a6b4c2120
xFreed0m <roei.sm@gmail.com>
2019-07-23 14:33:14 +0300

    packages update

--- wifi_plug.py
+++ wifi_plug.py
@@ -1,31 +1,51 @@
-# wifi_plug is a python script made to automate installation of wifi penteration tools
+# wifi_plug is a python script made to automate installation of wifi penetration tools
 # I don't own any of the used tools, but i thank their authors very much
 # Written by @x_Freed0m
 import os
-import sys
-from subprocess import STDOUT, check_call, Popen
-import shlex, subprocess
 import socket
+import subprocess
+import sys
+from subprocess import STDOUT
+
 from termcolor import colored
-import signal
+
+apt_pkgs = ['aircrack-ng', 'wifite', 'ettercap', 'sslstrip', 'isc-dhcp-server',
+            'dhcp-server', 'dsniff', 'reaver', 'bully', 'asleap']  # packages from apt-get
+git_pkgs = ['https://github.com/v1s1t0r1sh3r3/airgeddon/archive/master.zip',
+            'https://github.com/wi-fi-analyzer/fluxion/archive/master.zip',
+            'https://github.com/gobiasinfosec/Wireless_Query/archive/master.zip',
+            'https://github.com/H0nus/RogueSploit/archive/master.zip',
+            'https://github.com/P0cL4bs/WiFi-Pumpkin/archive/master.zip',
+            'https://github.com/wifiphisher/wifiphisher/archive/master.zip',
+            'https://github.com/s0lst1c3/eaphammer/archive/master.zip',
+            'https://github.com/entropy1337/infernal-twin/archive/master.zip',
+            'https://github.com/OpenSecurityResearch/hostapd-wpe/archive/master.zip',
+            'https://github.com/chrizator/netattack/archive/master.zip',
+            'https://github.com/hkm/whoishere.py/archive/master.zip',
+            'https://github.com/InfamousSYN/rogue/archive/master.zip',
+            'https://github.com/sensepost/mana/archive/master.zip',
+            'https://github.com/elkentaro/KismetMobileDashboard/archive/master.zip',
+            'https://github.com/wpatoolkit/Cap-Converter/archive/master.zip',
+            'https://github.com/tehw0lf/airbash/archive/master.zip',
+            'https://github.com/hashcat/hashcat-utils/archive/master.zip']
 
 
-apt_pkgs = ['aircrack-ng', 'wifite'] # packages from apt-get
-git_pkgs = ['https://github.com/v1s1t0r1sh3r3/airgeddon/archive/master.zip', 'https://github.com/wi-fi-analyzer/fluxion/archive/master.zip', 'https://github.com/gobiasinfosec/Wireless_Query/archive/master.zip', 'https://github.com/H0nus/RogueSploit/archive/master.zip', 'https://github.com/P0cL4bs/WiFi-Pumpkin/archive/master.zip', 'https://github.com/wifiphisher/wifiphisher/archive/master.zip', 'https://github.com/s0lst1c3/eaphammer/archive/master.zip', 'https://github.com/entropy1337/infernal-twin/archive/master.zip', 'https://github.com/OpenSecurityResearch/hostapd-wpe/archive/master.zip', 'https://github.com/chrizator/netattack/archive/master.zip', 'https://github.com/hkm/whoishere.py/archive/master.zip', 'https://github.com/InfamousSYN/rogue/archive/master.zip', 'https://github.com/sensepost/mana/archive/master.zip', 'https://github.com/elkentaro/KismetMobileDashboard/archive/master.zip', 'https://github.com/wpatoolkit/Cap-Converter/archive/master.zip', 'https://github.com/tehw0lf/airbash/archive/master.zip', 'https://github.com/hashcat/hashcat-utils/archive/master.zip'] # packges from github
+# Packages from github
 
-def internet_check(host="8.8.8.8", port=53, timeout=5): # checking the host has internet connection
+
+def internet_check(host="8.8.8.8", port=53, timeout=5):  # checking the host has internet connection
     try:
         socket.setdefaulttimeout(timeout)
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
-        print colored("[+] Internet access verified, moving forward" ,'green')
+        print colored("[+] Internet access verified, moving forward", 'green')
         return True
     except Exception as ex:
         print colored("[-] Can't connect to the internet.\n" + ex.message, 'red')
-        return False
         sys.exit(1)
 
+
 def installer():
-    if internet_check() == True:
+    if internet_check():
         try:
             print colored("[*] Updating the repositories...", 'green')
             os.system('apt-get update')
@@ -33,29 +53,40 @@ def installer():
         except KeyboardInterrupt:
             print colored("[-]halted by CTRL+C", 'red')
             sys.exit(1)
-        for pkg in apt_pkgs: # installing apt-get packages
+        for pkg in apt_pkgs:  # installing apt-get packages
             try:
-                print colored("[*] Installing "+ pkg, 'green')
-                proc = subprocess.Popen('apt-get install -y ' + pkg, shell=True, stdin=None, stdout=open(os.devnull, "wb"), stderr=STDOUT, executable="/bin/bash")
+                print colored("[*] Installing " + pkg, 'green')
+                proc = subprocess.Popen('apt-get install -y ' + pkg, shell=True, stdin=None,
+                                        stdout=open(os.devnull, "wb"), stderr=STDOUT,
+                                        executable="/bin/bash")
                 proc.wait()
                 print colored("[+] Done installing " + pkg + " moving forward...")
             except KeyboardInterrupt:
                 print colored("[-]halted by CTRL+C", 'red')
                 sys.exit(1)
-        for pkg in git_pkgs: # installing github packages
+        for pkg in git_pkgs:  # installing github packages
             try:
-                proc = subprocess.Popen("wget " + pkg, shell=True, stdin=None, stdout=open(os.devnull, "wb"), stderr=STDOUT, executable="/bin/bash")
+                proc = subprocess.Popen("wget " + pkg, shell=True, stdin=None,
+                                        stdout=open(os.devnull, "wb"), stderr=STDOUT,
+                                        executable="/bin/bash")
                 proc.wait()
-                proc = subprocess.Popen('unzip master.zip', shell=True, stdin=None, stdout=open(os.devnull, "wb"), stderr=STDOUT, executable="/bin/bash")
+                proc = subprocess.Popen('unzip master.zip', shell=True, stdin=None,
+                                        stdout=open(os.devnull, "wb"), stderr=STDOUT,
+                                        executable="/bin/bash")
                 proc.wait()
-                proc = subprocess.Popen('rm master.zip', shell=True, stdin=None, stdout=open(os.devnull, "wb"), stderr=STDOUT, executable="/bin/bash")
+                proc = subprocess.Popen('rm master.zip', shell=True, stdin=None,
+                                        stdout=open(os.devnull, "wb"), stderr=STDOUT,
+                                        executable="/bin/bash")
+                proc.wait()
                 print colored("[+] Done downloading and extracting " + pkg, 'green')
             except KeyboardInterrupt:
                 print colored("[-]halted by CTRL+C", 'red')
                 sys.exit(1)
             print colored("[+] All done! have fun!", 'green')
+            sys.exit(0)
     else:
         print colored("[-] Can't connect to the internet.\n", 'red')
+
 
 def help():
     print "This script will use apt-get in order to install"
@@ -66,11 +97,13 @@ def help():
     print "python wifi_plug.py"
     sys.exit()
 
+
 def main():
     if len(sys.argv) == 1:
         installer()
     else:
         help()
 
+
 if __name__ == '__main__':
-     main()
+    main()
